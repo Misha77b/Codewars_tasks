d2fdeea6a44d8c225f883930fcc528a4e8ad3fb3
Misha <misha77b@gmail.com>
2025-03-11 23:15:46 +0200

    highAndLow task solution is found

--- Highest and Lowest JS/script.js
+++ Highest and Lowest JS/script.js
@@ -1,1 +1,18 @@
 console.log("Hello, world!");
+
+function highAndLow(numbers) {
+	let strArr = numbers
+		.split(" ")
+		.map(Number)
+		.sort((a, b) => a - b);
+
+	let result = `${strArr[strArr.length - 1]} ${strArr[0]}`;
+
+	console.log(numbers.split(" "));
+
+	return result;
+}
+
+console.log(highAndLow("1 2 3 4 5"));
+console.log(highAndLow("1 2 -3 4 5"));
+console.log(highAndLow("1 9 3 4 -5"));
