1906ed240a21f67b617d5c4975c3fd96dde0906f
Misha <misha77b@gmail.com>
2025-03-12 22:56:16 +0200

    Complementary DNA task is done

--- Complementary DNA/script.js
+++ Complementary DNA/script.js
@@ -1,1 +1,30 @@
 console.log("Hello, world!");
+
+function dnaStrand(dna) {
+	let strand = "";
+
+	for (char of dna) {
+		switch (char) {
+			case "A":
+				strand += "T";
+				break;
+			case "T":
+				strand += "A";
+				break;
+			case "C":
+				strand += "G";
+				break;
+			case "G":
+				strand += "C";
+				break;
+			default:
+				return;
+		}
+	}
+
+	return strand;
+}
+
+console.log("Result is:", dnaStrand("AAAA"), "TTTT", "String AAAA is");
+console.log("Result is:", dnaStrand("ATTGC"), "TAACG", "String ATTGC is");
+console.log("Result is:", dnaStrand("GTAT"), "CATA", "String GTAT is");
